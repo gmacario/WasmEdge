de940a213cc806fb3ca9cde7e82b5d719385f556
Lîm Tsú-thuàn <dannypsnl@secondstate.io>
2024-11-07 11:51:33 +0800

    [Component] ValComp, add Enum
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@secondstate.io>

--- lib/loader/ast/component/component_type.cpp
+++ lib/loader/ast/component/component_type.cpp
@@ -145,7 +145,7 @@ Expect<void> Loader::loadType(Flags &Ty) {
   }
 }
 
-Expect<void> Loader::loadType(Enum &Ty) {
+Expect<void> Loader::loadType(EnumTy &Ty) {
   if (auto Res = loadVec<TypeSection>(
           Ty.getLabels(), [this](std::string Label) -> Expect<void> {
             return loadLabel(Label);
@@ -252,7 +252,7 @@ Expect<void> Loader::loadType(DefType &Ty) {
     }
     break;
   case 0x6d:
-    if (auto Res = loadType(Ty.emplace<DefValType>().emplace<Enum>()); !Res) {
+    if (auto Res = loadType(Ty.emplace<DefValType>().emplace<EnumTy>()); !Res) {
       spdlog::error(ErrInfo::InfoAST(ASTNodeAttr::DefType));
       return Unexpect(Res);
     }
