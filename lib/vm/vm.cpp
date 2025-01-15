29fd416076d82c15fbb3766ae738a366d684297e
Lîm Tsú-thuàn <dannypsnl@secondstate.io>
2025-01-15 15:17:09 +0800

    drafting new InterfaceType
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@secondstate.io>

--- lib/vm/vm.cpp
+++ lib/vm/vm.cpp
@@ -335,10 +335,10 @@ VM::unsafeRunWasmFile(const AST::Module &Module, std::string_view Func,
   return Unexpect(ErrCode::Value::WrongInstanceAddress);
 }
 
-Expect<std::vector<std::pair<ValInterface, ValType>>>
+Expect<std::vector<std::pair<ValInterface, InterfaceType>>>
 VM::unsafeExecute(const Runtime::Instance::ComponentInstance *CompInst,
                   std::string_view Func, Span<const ValInterface> Params,
-                  Span<const ValType> ParamTypes) {
+                  Span<const InterfaceType> ParamTypes) {
   // Find exported function by name.
   Runtime::Instance::Component::FunctionInstance *FuncInst =
       CompInst->findFuncExports(Func);
@@ -351,7 +351,7 @@ VM::unsafeExecute(const Runtime::Instance::ComponentInstance *CompInst,
     }
     return Unexpect(Res);
   } else {
-    return Res;
+    return *Res;
   }
 }
 
@@ -543,9 +543,9 @@ VM::unsafeExecute(std::string_view Func, Span<const ValVariant> Params,
   return Unexpect(ErrCode::Value::WrongInstanceAddress);
 }
 
-Expect<std::vector<std::pair<ValInterface, ValType>>>
+Expect<std::vector<std::pair<ValInterface, InterfaceType>>>
 VM::unsafeExecute(std::string_view Func, Span<const ValInterface> Params,
-                  Span<const ValType> ParamTypes) {
+                  Span<const InterfaceType> ParamTypes) {
   if (ActiveCompInst) {
     return unsafeExecute(ActiveCompInst.get(), Func, Params, ParamTypes);
   }
@@ -554,10 +554,10 @@ VM::unsafeExecute(std::string_view Func, Span<const ValInterface> Params,
   return Unexpect(ErrCode::Value::WrongInstanceAddress);
 }
 
-Expect<std::vector<std::pair<ValInterface, ValType>>>
+Expect<std::vector<std::pair<ValInterface, InterfaceType>>>
 VM::unsafeExecute(std::string_view CompName, std::string_view Func,
                   Span<const ValInterface> Params,
-                  Span<const ValType> ParamTypes) {
+                  Span<const InterfaceType> ParamTypes) {
   // Find component instance by name.
   const auto *FindCompInst = StoreRef.findComponent(CompName);
   if (FindCompInst != nullptr) {
@@ -630,24 +630,24 @@ VM::asyncExecute(std::string_view ModName, std::string_view Func,
           std::vector(ParamTypes.begin(), ParamTypes.end())};
 }
 
-Async<Expect<std::vector<std::pair<ValInterface, ValType>>>>
+Async<Expect<std::vector<std::pair<ValInterface, InterfaceType>>>>
 VM::asyncExecute(std::string_view Func, Span<const ValInterface> Params,
-                 Span<const ValType> ParamTypes) {
-  Expect<std::vector<std::pair<ValInterface, ValType>>> (VM::*FPtr)(
-      std::string_view, Span<const ValInterface>, Span<const ValType>) =
+                 Span<const InterfaceType> ParamTypes) {
+  Expect<std::vector<std::pair<ValInterface, InterfaceType>>> (VM::*FPtr)(
+      std::string_view, Span<const ValInterface>, Span<const InterfaceType>) =
       &VM::execute;
   return {FPtr, *this, std::string(Func),
           std::vector(Params.begin(), Params.end()),
           std::vector(ParamTypes.begin(), ParamTypes.end())};
 }
 
-Async<Expect<std::vector<std::pair<ValInterface, ValType>>>>
+Async<Expect<std::vector<std::pair<ValInterface, InterfaceType>>>>
 VM::asyncExecute(std::string_view CompName, std::string_view Func,
                  Span<const ValInterface> Params,
-                 Span<const ValType> ParamTypes) {
-  Expect<std::vector<std::pair<ValInterface, ValType>>> (VM::*FPtr)(
+                 Span<const InterfaceType> ParamTypes) {
+  Expect<std::vector<std::pair<ValInterface, InterfaceType>>> (VM::*FPtr)(
       std::string_view, std::string_view, Span<const ValInterface>,
-      Span<const ValType>) = &VM::execute;
+      Span<const InterfaceType>) = &VM::executeComponent;
   return {FPtr,
           *this,
           std::string(CompName),
@@ -691,7 +691,13 @@ VM::unsafeGetFunctionList() const {
         Map.emplace_back(Func.first, FuncType);
       }
     });
-  } else if (ActiveCompInst) {
+  }
+  return Map;
+}
+std::vector<std::pair<std::string, const AST::Component::FunctionType &>>
+VM::unsafeGetCompFunctionList() const {
+  std::vector<std::pair<std::string, const AST::Component::FunctionType &>> Map;
+  if (ActiveCompInst) {
     return ActiveCompInst->getFuncExports();
   }
   return Map;
